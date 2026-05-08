5f1128585446e71debb20dea348f5ee8339c76ff
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2026-05-08 17:36:18 +0200

    solution: awkwardparty

---
+++ tasks/awkward_party/src/main.rs
@@ -0,0 +1,64 @@
+//{"name":"Awkward Party","group":"Kattis","url":"https://open.kattis.com/problems/awkwardparty","interactive":false,"timeLimit":1000,"tests":[{"input":"4\n1 2 3 1\n","output":"3\n"},{"input":"3\n1 2 3\n","output":"3\n"}],"testType":"single","input":{"type":"stdin","fileName":null,"pattern":null},"output":{"type":"stdout","fileName":null,"pattern":null},"languages":{"java":{"taskClass":"AwkwardParty"}}}
+
+use std::collections::HashMap;
+
+use algo_lib::io::input::Input;
+use algo_lib::io::output::Output;
+use algo_lib::misc::test_type::TaskType;
+
+use algo_lib::misc::test_type::TestType;
+
+type PreCalc = ();
+
+/// use the fact that 'map.insert' returns an Option of the old value
+fn solve(input: &mut Input, out: &mut Output, _test_case: usize, _data: &mut PreCalc) {
+    let n = input.read_size();
+
+    let mut map = HashMap::new();
+    let mut awkwards = n;
+
+    for i in 0..n {
+        let id = input.read_unsigned();
+        if let Some(old_at) = map.insert(id, i) {
+            awkwards = awkwards.min(i - old_at);
+        }
+    }
+    out.print_line(awkwards);
+}
+
+pub static TEST_TYPE: TestType = TestType::Single;
+pub static TASK_TYPE: TaskType = TaskType::Classic;
+
+pub(crate) fn run(mut input: Input, mut output: Output) -> bool {
+    let mut pre_calc = ();
+
+    match TEST_TYPE {
+        TestType::Single => solve(&mut input, &mut output, 1, &mut pre_calc),
+        TestType::MultiNumber => {
+            let t = input.read();
+            for i in 1..=t {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+            }
+        }
+        TestType::MultiEof => {
+            let mut i = 1;
+            while input.peek().is_some() {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+                i += 1;
+            }
+        }
+    }
+    output.flush();
+    match TASK_TYPE {
+        TaskType::Classic => input.is_empty(),
+        TaskType::Interactive => true,
+    }
+}
+
+//START MAIN
+mod tester;
+
+fn main() {
+    tester::run_tests();
+}
+//END MAIN
