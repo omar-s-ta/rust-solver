acd4be2d6558621f21aa8bcec4835ec71ab4a4d5
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2026-05-28 15:36:03 +0200

    solution: candydivision

---
+++ tasks/candy_division/src/main.rs
@@ -0,0 +1,65 @@
+//{"name":"Candy Division","group":"Kattis","url":"https://open.kattis.com/problems/candydivision","interactive":false,"timeLimit":1000,"tests":[{"input":"30\n","output":"0 1 2 4 5 9 14 29\n"}],"testType":"single","input":{"type":"stdin","fileName":null,"pattern":null},"output":{"type":"stdout","fileName":null,"pattern":null},"languages":{"java":{"taskClass":"CandyDivision"}}}
+
+use std::collections::BTreeSet;
+
+use algo_lib::io::input::Input;
+use algo_lib::io::output::Output;
+use algo_lib::misc::test_type::TaskType;
+
+use algo_lib::misc::test_type::TestType;
+
+type PreCalc = ();
+
+fn solve(input: &mut Input, out: &mut Output, _test_case: usize, _data: &mut PreCalc) {
+    let n = input.read_u64();
+
+    let mut i = 1;
+    let mut set = BTreeSet::new();
+
+    while i * i <= n {
+        if n.is_multiple_of(i) {
+            set.insert((n / i) - 1);
+            set.insert(i - 1);
+        }
+        i += 1;
+    }
+
+    out.print_iter_one_line(set.iter());
+}
+
+pub static TEST_TYPE: TestType = TestType::Single;
+pub static TASK_TYPE: TaskType = TaskType::Classic;
+
+pub(crate) fn run(mut input: Input, mut output: Output) -> bool {
+    let mut pre_calc = ();
+
+    match TEST_TYPE {
+        TestType::Single => solve(&mut input, &mut output, 1, &mut pre_calc),
+        TestType::MultiNumber => {
+            let t = input.read();
+            for i in 1..=t {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+            }
+        }
+        TestType::MultiEof => {
+            let mut i = 1;
+            while input.peek().is_some() {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+                i += 1;
+            }
+        }
+    }
+    output.flush();
+    match TASK_TYPE {
+        TaskType::Classic => input.is_empty(),
+        TaskType::Interactive => true,
+    }
+}
+
+//START MAIN
+mod tester;
+
+fn main() {
+    tester::run_tests();
+}
+//END MAIN
