e44b87dda0be2f4a7d446173d83a5a2b65a29ff7
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2026-06-01 17:21:29 +0200

    improvement: transparent_wrapper macro

--- algo_lib/src/misc/mod.rs
+++ algo_lib/src/misc/mod.rs
@@ -1,3 +1,4 @@
 pub mod lcg;
 pub mod run_parallel;
 pub mod test_type;
+pub mod transparent_wrapper;
---
+++ algo_lib/src/misc/transparent_wrapper.rs
@@ -0,0 +1,9 @@
+#[macro_export]
+macro_rules! transparent_wrapper {
+    ($name:ident $(<$($par:ident $(,)?)+>)? = $t:ty $(, derive $($d:ty $(,)?)+)?) => {
+        $(#[derive($($d,)+)])?
+        pub struct $name $(<$($par,)+>)? ($t);
+        impl $(<$($par,)+>)? Deref for $name $(<$($par,)+>)? { type Target = $t; fn deref(&self) -> & Self::Target { & self.0 } }
+        impl $(<$($par,)+>)? DerefMut for $name $(<$($par,)+>)? { fn deref_mut(& mut self) -> & mut Self::Target { & mut self.0 } }
+    };
+}
