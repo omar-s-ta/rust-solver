34509dcae26aa3548a222d44fd1d553f63566075
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2026-06-09 17:27:23 +0200

    solution: kattisquest

---
+++ tasks/kattis_s_quest/src/main.rs
@@ -0,0 +1,94 @@
+use std::cmp::Reverse;
+use std::collections::BTreeSet;
+
+use algo_lib::io::input::Input;
+use algo_lib::io::output::Output;
+use algo_lib::misc::test_type::TaskType;
+
+use algo_lib::misc::test_type::TestType;
+use algo_lib::string::str::StrReader;
+
+type PreCalc = ();
+
+fn solve(input: &mut Input, out: &mut Output, _test_case: usize, _data: &mut PreCalc) {
+    let n = input.read_u32();
+
+    let mut set = BTreeSet::new();
+    for i in 0..n {
+        let cmd = input.read_str();
+        if cmd[0] == b'a' {
+            set.insert((Reverse(input.read_u32()), Reverse(input.read_u32()), i));
+        } else {
+            let mut x = input.read_u32();
+            let mut gold = 0_u64;
+            loop {
+                let next = set
+                    .range((Reverse(x), Reverse(u32::MAX), 0)..)
+                    .next()
+                    .copied();
+
+                match next {
+                    Some(elem) => {
+                        let (Reverse(e), Reverse(g), _) = elem;
+                        if let Some(r) = x.checked_sub(e) {
+                            gold += g as u64;
+                            x = r;
+                            set.remove(&elem);
+                        } else {
+                            break;
+                        }
+                    }
+                    None => break,
+                }
+            }
+            out.print_line(gold);
+        }
+    }
+}
+
+pub static TEST_TYPE: TestType = TestType::Single;
+pub static TASK_TYPE: TaskType = TaskType::Classic;
+
+pub(crate) fn run(mut input: Input, mut output: Output) -> bool {
+    eprint!("\x1B[33m\x1B[03m");
+
+    let mut pre_calc = ();
+
+    match TEST_TYPE {
+        TestType::Single => solve(&mut input, &mut output, 1, &mut pre_calc),
+        TestType::MultiNumber => {
+            let t = input.read();
+            for i in 1..=t {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+            }
+        }
+        TestType::MultiEof => {
+            let mut i = 1;
+            while input.peek().is_some() {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+                i += 1;
+            }
+        }
+        _ => {
+            unreachable!();
+        }
+    }
+    eprint!("\x1B[0m");
+    output.flush();
+    input.is_run_done()
+}
+
+#[cfg(feature = "local")]
+mod tester;
+
+#[cfg(feature = "local")]
+fn main() {
+    tester::run_tests();
+}
+
+#[cfg(not(feature = "local"))]
+fn main() {
+    let input = algo_lib::io::input::Input::stdin();
+    let output = algo_lib::io::output::Output::stdout();
+    run(input, output);
+}
