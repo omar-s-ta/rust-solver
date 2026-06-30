d7c4a1e23d118109be95693b005d7aaca27b0b4a
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2026-06-30 16:32:02 +0200

    solution: chopping_wood

---
+++ archive/cp-4/ch-2/Kattis/chopping_wood.rs
@@ -0,0 +1,73 @@
+use algo_lib::graph::prufer_code::PruferCode;
+use algo_lib::io::input::Input;
+use algo_lib::io::output::Output;
+use algo_lib::misc::test_type::TaskType;
+
+use algo_lib::misc::test_type::TestType;
+
+type PreCalc = ();
+
+fn solve(input: &mut Input, out: &mut Output, _test_case: usize, _data: &mut PreCalc) {
+    let n = input.read_size();
+    let code = input.read_size_vec(n);
+
+    // last element remaining
+    // is always the largest label
+    // if it is not -> bad code
+    if code[n - 1] != n + 1 {
+        out.print_line("Error");
+        return;
+    }
+
+    let code = code[..n - 1].iter().map(|v| v - 1).collect::<Vec<_>>();
+    let edges = PruferCode::decode(&code).into_iter().map(|(t, _)| t + 1);
+
+    out.print_iter_per_line(edges);
+}
+
+pub static TEST_TYPE: TestType = TestType::Single;
+pub static TASK_TYPE: TaskType = TaskType::Classic;
+
+pub(crate) fn run(mut input: Input, mut output: Output) -> bool {
+    eprint!("\x1B[33m\x1B[03m");
+
+    let mut pre_calc = ();
+
+    match TEST_TYPE {
+        TestType::Single => solve(&mut input, &mut output, 1, &mut pre_calc),
+        TestType::MultiNumber => {
+            let t = input.read();
+            for i in 1..=t {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+            }
+        }
+        TestType::MultiEof => {
+            let mut i = 1;
+            while input.peek().is_some() {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+                i += 1;
+            }
+        }
+        _ => {
+            unreachable!();
+        }
+    }
+    eprint!("\x1B[0m");
+    output.flush();
+    input.is_run_done()
+}
+
+#[cfg(feature = "local")]
+mod tester;
+
+#[cfg(feature = "local")]
+fn main() {
+    tester::run_tests();
+}
+
+#[cfg(not(feature = "local"))]
+fn main() {
+    let input = algo_lib::io::input::Input::stdin();
+    let output = algo_lib::io::output::Output::stdout();
+    run(input, output);
+}
